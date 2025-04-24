8783296ee825d20d4c88bf91f24a4a3c42b92f05
Rajan Poudel <raajan.works@gmail.com>
2025-04-24 20:38:39 +0545

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,23 @@
+/* Write a C program to calculate the factorial of a number using recursion. */
+
+#include <stdio.h>
+
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.\n");
+    else
+        printf("Factorial of %d is %lld\n", num, factorial(num));
+
+    return 0;
+}
