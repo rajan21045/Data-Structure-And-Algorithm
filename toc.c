ed445a90acb74194d19f4b1e795f0b3f30d51f66
Rajan Poudel <raajan.works@gmail.com>
2025-05-11 09:13:15 +0545

    Update toc.c

--- toc.c
+++ toc.c
@@ -1,17 +1,25 @@
-/* Write a C program to solve the Tower of Hanoi problem using recursion. */
+/* Write a C program to solve the Tower of Hanoi problem using 
+recursion. */
+
 #include<stdlib.h>
 #include <stdio.h>
+
 void hanoi(int , char , char , char );
 int main() {
+    
     // Your code goes here
     hanoi(3,'A','B','C');
     return 0;
 }
+
 void hanoi(int n, char rodFrom, char rodMiddle, char rodTo) 
+
 {
     if (n == 1) {
+        
         printf("Move disk 1 from rod %c to rod %c \n", rodFrom, rodTo);
         return;
+        
         }
 
             hanoi(n-1, rodFrom, rodTo, rodMiddle);
